2b6c170293d16ca6079f1bb20a61886f99d2d6ec
gitboyzorro5 <kibetleonard8@gmail.com>
2021-03-26 17:49:19 +0300

    updated lesson1

--- lesson1.R
+++ lesson1.R
@@ -1,4 +1,29 @@
 data <- read.csv("allteams20202021SOT.csv")
 summary(data)
 epl <- subset(data,Div == "E0")
-summary(epl)
+laliga <- subset(data,Div == "SP1")
+summary(laliga)
+is.data.frame(data)
+head(data)
+tail(data)
+str(data)
+nrow(data)
+ncol(data)
+colnames(data)
+rownames(data)
+first_20 <- head(data[['HomeTeam']],20)
+first_20
+data$FTR
+data[1,3]
+data[,1]
+epl[epl$FTHG > 3,]
+transform(data,FTHG = FTHG + 1)
+install.packages('xlsx')
+library('xlsx')
+myoddsdata <- read.xlsx("myodds.xlsx",sheetIndex = 1)
+summary(myoddsdata)
+install.packages('dplyr')
+library('dplyr')
+a <- data.frame(a = 1:2, b = 2:3, c = 4:5)
+b <- data.frame(a = 5:6, b = 6:7 , c = 7:8)
+rbind(a,b)

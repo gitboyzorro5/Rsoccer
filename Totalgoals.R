83a7f01e19a59636dbf67a883d2c4af30d38b786
gitboyzorro5 <kibetleonard8@gmail.com>
2021-03-31 07:49:53 +0300

    added ov25 column

--- Totalgoals.R
+++ Totalgoals.R
@@ -4,3 +4,18 @@ total_goals <- data$FTHG + data$FTAG
 #assign column to data frame
 data <- cbind(data,total_goals)
 head(data)
+colnames(data)
+newdata <- subset(data, select = -c(margin,margin))
+head(newdata)
+colnames(newdata)
+newdata$OV25 <- ifelse(newdata$total_goals >= 3,"Y","N")
+colnames(newdata)
+head(newdata)
+library('dplyr')
+summary_div <- newdata %>% group_by(Div) %>% summarise_if(is.numeric,sum)
+nrow(newdata[newdata$Div == "E0",])
+write.csv(summary_div,"sumall.csv")
+divisions <- unique(newdata$Div)
+index <- seq(1,22)
+
+

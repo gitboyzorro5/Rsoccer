03b3f3e4701b1f10a971e08c21ae7fd998ca527b
gitboyzorro5 <kibetleonard8@gmail.com>
2021-04-08 08:16:12 +0300

    08/04/2021 update

--- TGmatrix.R
+++ TGmatrix.R
@@ -4,11 +4,23 @@ data_2021$TG <- total_goals
 cbind(data_2021, total_goals)
 B1 <- subset(data_2021, Div == "B1")
 #library('lubridate')
-with(B1, tapply(TG, list(HomeTeam, Date), FUN = mean))
+library('dplyr')
+ hometeam_tg <- with(B1, tapply(TG, list(HomeTeam, Date), FUN = mean))
+ awayteam_tg <- with(B1, tapply(TG, list(AwayTeam, Date), FUN = mean))
+
 testdata_h <- tapply(B1$TG, B1[c("HomeTeam", "Date")],mean)
 testdata_a <- tapply(B1$TG, B1[c("AwayTeam", "Date")],mean)
-testdata_h[is.na(testdata_h)] <- ""
-testdata_a[is.na(testdata_a)] <- ""
-merge(testdata_h,testdata_a,)
-testdata_h[[2,2]]
 
+hometeam_tg[is.na(hometeam_tg)] <- ""
+awayteam_tg[is.na(awayteam_tg)] <- ""
+
+write.csv(hometeam_tg, 'hometg.csv')
+write.csv(awayteam_tg, 'awaytg.csv')
+
+mergedtest <- merge(hometeam_tg, awayteam_tg,by = 'row.names',all.hometeam_tg = awayteam_tgl,no.dups = T)
+write.csv(mergedtest,'merged.csv')
+?merge
+Sys.setenv(JAVA_HOME ="C:\\Program Files\\Java\\jre1.8.0_281")
+library('xlsx')
+write.xlsx(hometeam_tg,'hometg.xlsx',sheetName = "B1h")
+write.xlsx(awayteam_tg,'hometg.xlsx',sheetName = "B1h", append = TRUE)

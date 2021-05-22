b0719c11891c050f8cf1b905548877558e936efd
gitboyzorro5 <kibetleonard8@gmail.com>
2021-05-22 14:01:47 +0300

    updated basicanalytics

--- BasicAnalytics.R
+++ BasicAnalytics.R
@@ -79,6 +79,8 @@ rownames(B1_fixtures) <- NULL
 B1_fixtures$Div <- "B1"
 B1_fixtures <- B1_fixtures[,c(3,1,2)]
 
+B1_fixtures$Date <- sqldf("SELECT B1.Date from B1 INNER JOIN B1_fixtures ON B1.Hometeam = B1_fixtures.HomeTeam_b1 AND B1.AwayTeam = B1_fixtures.AwayTeam_b1")
+
 B1_fixtures$avg_HG_b1 <- b1_avg_HG
 
 B1_fixtures$b1_homeas <- rep(b1_home_as,each = length(b1_teams)-1)
@@ -217,11 +219,161 @@ B1_fixtures$b1_un25 <- (
 )
 
 B1_fixtures$b1_un25 <- percent(B1_fixtures$b1_un25, accuracy = 0.1)
+#######################################################################################################################
+#D1
+HomeTeam_d1 <- rep(d1_teams, each = length(d1_teams))
+AwayTeam_d1 <- rep(d1_teams, length(d1_teams))
+D1_fixtures <- cbind(HomeTeam_d1,AwayTeam_d1)
+D1_fixtures <- as.data.frame(D1_fixtures)
+D1_fixtures <- D1_fixtures[!D1_fixtures$HomeTeam_d1 == D1_fixtures$AwayTeam_d1,]
+rownames(D1_fixtures) <- NULL
+D1_fixtures$Div <- "D1"
+D1_fixtures <- D1_fixtures[,c(3,1,2)]
+
+D1_fixtures$Date
+
+D1_fixtures$Date <- sqldf("SELECT D1.Date from D1 LEFT OUTER JOIN D1_fixtures ON D1.Hometeam = D1_fixtures.HomeTeam_d1 AND D1.AwayTeam = D1_fixtures.AwayTeam_d1")
+
+D1_fixtures$avg_HG_d1 <- d1_avg_HG
+
+D1_fixtures$d1_homeas <- rep(d1_home_as,each = length(d1_teams)-1)
+
+d1_awayds_lookup <- cbind(d1_teams,d1_away_ds)
+
+d1_awayds_lookup <- as.data.frame(d1_awayds_lookup)
+
+colnames(d1_awayds_lookup) <- c("AwayTeam_d1","d1_awayds")
 
 
+require('RH2')
+D1_fixtures$d1_awayds <- sqldf("SELECT d1_awayds_lookup.d1_awayds FROM d1_awayds_lookup INNER JOIN D1_fixtures ON d1_awayds_lookup.AwayTeam_d1 = D1_fixtures.AwayTeam_d1")
+
+D1_fixtures$avg_AG_d1 <- d1_avg_AG
+
+d1_awayas_lookup <- cbind(d1_teams,d1_away_as)
+
+d1_awayas_lookup <- as.data.frame(d1_awayas_lookup)
+
+colnames(d1_awayas_lookup) <- c("AwayTeam_d1","d1_awayas")
 
 
+D1_fixtures$d1_awayas <- sqldf("SELECT d1_awayas_lookup.d1_awayas FROM d1_awayas_lookup INNER JOIN D1_fixtures ON d1_awayas_lookup.AwayTeam_d1 = D1_fixtures.AwayTeam_d1")
 
+D1_fixtures$d1_homeds <- rep(d1_home_ds,each = length(d1_teams)-1)
+
+D1_fixtures$d1_awayds <- as.numeric(unlist(D1_fixtures$d1_awayds))
+#xGH
+D1_fixtures$d1_xGH <- D1_fixtures$avg_HG_d1 * D1_fixtures$d1_homeas * D1_fixtures$d1_awayds
+
+#xGA
+
+D1_fixtures$d1_awayas <- as.numeric(unlist(D1_fixtures$d1_awayas))
+
+D1_fixtures$d1_xGA <- D1_fixtures$avg_AG_d1 * D1_fixtures$d1_awayas * D1_fixtures$d1_homeds
+
+D1_fixtures$d1_0_0 <- round(stats::dpois(0,D1_fixtures$d1_xGH) * stats::dpois(0,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_1_0 <- round(stats::dpois(1,D1_fixtures$d1_xGH) * stats::dpois(0,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_0_1 <- round(stats::dpois(0,D1_fixtures$d1_xGH) * stats::dpois(1,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_1_1 <- round(stats::dpois(1,D1_fixtures$d1_xGH) * stats::dpois(1,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_2_0 <- round(stats::dpois(2,D1_fixtures$d1_xGH) * stats::dpois(0,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_0_2 <- round(stats::dpois(0,D1_fixtures$d1_xGH) * stats::dpois(2,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_2_2 <- round(stats::dpois(2,D1_fixtures$d1_xGH) * stats::dpois(2,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_2_1 <- round(stats::dpois(2,D1_fixtures$d1_xGH) * stats::dpois(1,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_1_2 <- round(stats::dpois(1,D1_fixtures$d1_xGH) * stats::dpois(2,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_3_3 <- round(stats::dpois(3,D1_fixtures$d1_xGH) * stats::dpois(3,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_3_0 <- round(stats::dpois(3,D1_fixtures$d1_xGH) * stats::dpois(0,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_3_1 <- round(stats::dpois(3,D1_fixtures$d1_xGH) * stats::dpois(1,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_3_2 <- round(stats::dpois(3,D1_fixtures$d1_xGH) * stats::dpois(2,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_0_3 <- round(stats::dpois(0,D1_fixtures$d1_xGH) * stats::dpois(3,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_1_3 <- round(stats::dpois(1,D1_fixtures$d1_xGH) * stats::dpois(3,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_2_3 <- round(stats::dpois(2,D1_fixtures$d1_xGH) * stats::dpois(3,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_4_4 <- round(stats::dpois(4,D1_fixtures$d1_xGH) * stats::dpois(4,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_4_0 <- round(stats::dpois(4,D1_fixtures$d1_xGH) * stats::dpois(0,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_4_1 <- round(stats::dpois(4,D1_fixtures$d1_xGH) * stats::dpois(1,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_4_2 <- round(stats::dpois(4,D1_fixtures$d1_xGH) * stats::dpois(2,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_4_3 <- round(stats::dpois(4,D1_fixtures$d1_xGH) * stats::dpois(3,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_0_4 <- round(stats::dpois(0,D1_fixtures$d1_xGH) * stats::dpois(4,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_1_4 <- round(stats::dpois(1,D1_fixtures$d1_xGH) * stats::dpois(4,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_2_4 <- round(stats::dpois(2,D1_fixtures$d1_xGH) * stats::dpois(4,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_3_4 <- round(stats::dpois(3,D1_fixtures$d1_xGH) * stats::dpois(4,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_5_5 <- round(stats::dpois(5,D1_fixtures$d1_xGH) * stats::dpois(5,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_5_0 <- round(stats::dpois(5,D1_fixtures$d1_xGH) * stats::dpois(0,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_5_1 <- round(stats::dpois(5,D1_fixtures$d1_xGH) * stats::dpois(1,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_5_2 <- round(stats::dpois(5,D1_fixtures$d1_xGH) * stats::dpois(2,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_5_3 <- round(stats::dpois(5,D1_fixtures$d1_xGH) * stats::dpois(3,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_5_4 <- round(stats::dpois(5,D1_fixtures$d1_xGH) * stats::dpois(4,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_0_5 <- round(stats::dpois(0,D1_fixtures$d1_xGH) * stats::dpois(5,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_1_5 <- round(stats::dpois(1,D1_fixtures$d1_xGH) * stats::dpois(5,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_2_5 <- round(stats::dpois(2,D1_fixtures$d1_xGH) * stats::dpois(5,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_3_5 <- round(stats::dpois(3,D1_fixtures$d1_xGH) * stats::dpois(5,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_4_5 <- round(stats::dpois(4,D1_fixtures$d1_xGH) * stats::dpois(5,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_6_6 <- round(stats::dpois(6,D1_fixtures$d1_xGH) * stats::dpois(6,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_6_0 <- round(stats::dpois(6,D1_fixtures$d1_xGH) * stats::dpois(0,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_6_1 <- round(stats::dpois(6,D1_fixtures$d1_xGH) * stats::dpois(1,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_6_2 <- round(stats::dpois(6,D1_fixtures$d1_xGH) * stats::dpois(2,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_6_3 <- round(stats::dpois(6,D1_fixtures$d1_xGH) * stats::dpois(3,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_6_4 <- round(stats::dpois(6,D1_fixtures$d1_xGH) * stats::dpois(4,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_6_5 <- round(stats::dpois(6,D1_fixtures$d1_xGH) * stats::dpois(5,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_0_6 <- round(stats::dpois(0,D1_fixtures$d1_xGH) * stats::dpois(6,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_1_6 <- round(stats::dpois(1,D1_fixtures$d1_xGH) * stats::dpois(6,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_2_6 <- round(stats::dpois(2,D1_fixtures$d1_xGH) * stats::dpois(6,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_3_6 <- round(stats::dpois(3,D1_fixtures$d1_xGH) * stats::dpois(6,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_4_6 <- round(stats::dpois(4,D1_fixtures$d1_xGH) * stats::dpois(6,D1_fixtures$d1_xGA), digits = 4)
+D1_fixtures$d1_5_6 <- round(stats::dpois(5,D1_fixtures$d1_xGH) * stats::dpois(6,D1_fixtures$d1_xGA), digits = 4)
+#Home win
+D1_fixtures$d1_H <- (
+  D1_fixtures$d1_1_0 + D1_fixtures$d1_2_0 + D1_fixtures$d1_2_1 + D1_fixtures$d1_3_0 + D1_fixtures$d1_3_1 +
+    D1_fixtures$d1_3_2 + D1_fixtures$d1_4_0 + D1_fixtures$d1_4_1 + D1_fixtures$d1_4_2 + D1_fixtures$d1_4_3 +
+    D1_fixtures$d1_5_0 + D1_fixtures$d1_5_1 + D1_fixtures$d1_5_2 + D1_fixtures$d1_5_3 + D1_fixtures$d1_5_4 +
+    D1_fixtures$d1_6_0 + D1_fixtures$d1_6_1 + D1_fixtures$d1_6_2 + D1_fixtures$d1_6_3 + D1_fixtures$d1_6_4 +
+    D1_fixtures$d1_6_5
+)
+
+D1_fixtures$d1_H <- percent(D1_fixtures$d1_H, accuracy = 0.1)
+
+#Draw
+D1_fixtures$d1_D <- (
+
+  D1_fixtures$d1_0_0 + D1_fixtures$d1_1_1 + D1_fixtures$d1_2_2 + D1_fixtures$d1_3_3 + D1_fixtures$d1_4_4 +
+    D1_fixtures$d1_5_5 + D1_fixtures$d1_6_6
+)
+
+D1_fixtures$d1_D <- percent(D1_fixtures$d1_D, accuracy = 0.1)
+
+#Away
+
+D1_fixtures$d1_A <- (
+  D1_fixtures$d1_0_1 + D1_fixtures$d1_0_2 + D1_fixtures$d1_1_2 + D1_fixtures$d1_0_3 + D1_fixtures$d1_1_3 +
+    D1_fixtures$d1_2_3 + D1_fixtures$d1_0_4 + D1_fixtures$d1_1_4 + D1_fixtures$d1_2_4 + D1_fixtures$d1_3_4 +
+    D1_fixtures$d1_0_5 + D1_fixtures$d1_1_5 + D1_fixtures$d1_2_5 + D1_fixtures$d1_3_5 + D1_fixtures$d1_4_5 +
+    D1_fixtures$d1_0_6 + D1_fixtures$d1_1_6 + D1_fixtures$d1_2_6 + D1_fixtures$d1_3_6 + D1_fixtures$d1_4_6 +
+    D1_fixtures$d1_5_6
+)
+
+D1_fixtures$d1_A <- percent(D1_fixtures$d1_A, accuracy = 0.1)
+
+#ov25
+
+D1_fixtures$d1_ov25 <- (
+  D1_fixtures$d1_2_1 + D1_fixtures$d1_1_2 + D1_fixtures$d1_2_2 + D1_fixtures$d1_3_0 + D1_fixtures$d1_3_1 +
+    D1_fixtures$d1_3_2 + D1_fixtures$d1_0_3 + D1_fixtures$d1_1_3 + D1_fixtures$d1_2_3 + D1_fixtures$d1_3_3 +
+    D1_fixtures$d1_4_0 + D1_fixtures$d1_4_1 + D1_fixtures$d1_4_2 + D1_fixtures$d1_4_3 + D1_fixtures$d1_0_4 +
+    D1_fixtures$d1_1_4 + D1_fixtures$d1_2_4 + D1_fixtures$d1_3_4 + D1_fixtures$d1_4_4 + D1_fixtures$d1_5_0 +
+    D1_fixtures$d1_5_1 + D1_fixtures$d1_5_2 + D1_fixtures$d1_5_3 + D1_fixtures$d1_5_4 + D1_fixtures$d1_0_5 +
+    D1_fixtures$d1_1_5 + D1_fixtures$d1_2_5 + D1_fixtures$d1_3_5 + D1_fixtures$d1_4_5 + D1_fixtures$d1_5_5 +
+    D1_fixtures$d1_6_0 + D1_fixtures$d1_6_1 + D1_fixtures$d1_6_2 + D1_fixtures$d1_6_3 + D1_fixtures$d1_6_4 +
+    D1_fixtures$d1_6_5 + D1_fixtures$d1_0_6 + D1_fixtures$d1_1_6 + D1_fixtures$d1_2_6 + D1_fixtures$d1_3_6 +
+    D1_fixtures$d1_4_6 + D1_fixtures$d1_5_6 + D1_fixtures$d1_6_6
+)
+D1_fixtures$d1_ov25 <- percent(D1_fixtures$d1_ov25, accuracy = 0.1)
+#un25
+D1_fixtures$d1_un25 <- (
+  D1_fixtures$d1_0_0 + D1_fixtures$d1_1_0 + D1_fixtures$d1_0_1 + D1_fixtures$d1_1_1 + D1_fixtures$d1_2_0 + D1_fixtures$d1_0_2
+)
+
+D1_fixtures$d1_un25 <- percent(D1_fixtures$d1_un25, accuracy = 0.1)
+
+D1_fixtures
 
 
 

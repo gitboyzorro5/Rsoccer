aabe14f4e02a062b6bd2bd2e2430e8b75523024a
gitboyzorro5 <kibetleonard8@gmail.com>
2022-02-04 17:27:47 +0300

    updated divisions

--- BasicAnalytics.R
+++ BasicAnalytics.R
@@ -711,35 +711,35 @@ write.xlsx(b1_roundmatrix,'b1_roundmatrix.xlsx')
 ##################################################################################################################
 ##################################################################################################################
 #b1_krounds is total rounds as per current season
-b1_krounds <- tail(unique(B1_rounds$b1_matchday),1)
-b1_goalscoredmatrix <- data.frame(matrix(nrow = length(b1_teams),ncol = b1_krounds))
-
-b1_goalsfor <- c()
-
-for(i_b1_krounds in 1:b1_krounds)
-{
-
-  for (i_b1_gs in 1:length(b1_teams))
-  {
-
-   b1_goalsfor[i_b1_gs] <- ifelse(B1_rounds$HomeTeam == b1_teams[i_b1_gs],B1_rounds$FTHG[B1_rounds$HomeTeam == b1_teams[i_b1_gs]],B1_rounds$FTAG[B1_rounds$AwayTeam == b1_teams[i_b1_gs]])
-
-  }
-
-  b1_goalscoredmatrix[,i_b1_krounds] <- as.data.frame(b1_goalsfor[])
-
-}
-
-b1_goalscoredmatrix <- cbind(b1_teams,b1_goalscoredmatrix)
-
-
-b1_goalsfor
-b1_goalscoredmatrix
-b1_teams
-head(B1,10)
-
-warnings()
-
+# b1_krounds <- tail(unique(B1_rounds$b1_matchday),1)
+# b1_goalscoredmatrix <- data.frame(matrix(nrow = length(b1_teams),ncol = b1_krounds))
+#
+# b1_goalsfor <- c()
+#
+# for(i_b1_krounds in 1:b1_krounds)
+# {
+#
+#   for (i_b1_gs in 1:length(b1_teams))
+#   {
+#
+#    b1_goalsfor[i_b1_gs] <-
+#
+#   }
+#
+#   b1_goalscoredmatrix[,i_b1_krounds] <- as.data.frame(b1_goalsfor[])
+#
+# }
+#
+# b1_goalscoredmatrix <- cbind(b1_teams,b1_goalscoredmatrix)
+#
+#
+# b1_goalsfor
+# b1_goalscoredmatrix
+# b1_teams
+# head(B1,10)
+#
+# warnings()
+#
 
 
 

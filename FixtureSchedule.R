acbe53a7f9e749a8e7655dc6441d02b315a7d520
gitboyzorro5 <kibetleonard8@gmail.com>
2022-03-04 17:05:43 +0300

    updated predictions

--- FixtureSchedule.R
+++ FixtureSchedule.R
@@ -72,6 +72,6 @@ all_schedule20212022 <- rbind(B1_schedule20212022,D1_schedule20212022,D2_schedul
 
 all_schedule20212022 <- all_schedule20212022[,c(7,5,6,3)]
 
-myfixtures <- all_schedule20212022[all_schedule20212022$Date >= '2022-02-25' & all_schedule20212022$Date <= '2022-02-28',]
+myfixtures <- all_schedule20212022[all_schedule20212022$Date >= '2022-03-03' & all_schedule20212022$Date <= '2022-03-08',]
 
 write.csv(myfixtures,'myfixtures.csv')
